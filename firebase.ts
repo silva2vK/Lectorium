ece56b6e756fdca90557acdff24cc021e4f23177
Silva <silvabell741@gmail.com>
2026-01-23 02:21:27 -0300

    feat: Update Firebase config
    Updates Firebase configuration to reflect the new project ID and related credentials for authentication services. Removes analytics import as it's not being used.

--- firebase.ts
+++ firebase.ts
@@ -1,21 +1,17 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
+// Configuração do Firebase (Apenas para Autenticação Google)
 const firebaseConfig = {
-  apiKey: "AIzaSyAROw7quwsp8lIKjnXnT9dhg6C3iwvepTc",
-  authDomain: "lectorium-v2.firebaseapp.com",
-  projectId: "lectorium-v2",
-  storageBucket: "lectorium-v2.firebasestorage.app",
-  messagingSenderId: "880199487015",
-  appId: "1:880199487015:web:547fdd08fbc51a57890802",
-  measurementId: "G-Q94QEDSTYC"
+  apiKey: "AIzaSyAt_O84vRAH4NbYzjXsYUFGXe82oR_SS4E",
+  authDomain: "historia-acessivel-76f55.firebaseapp.com",
+  projectId: "historia-acessivel-76f55",
+  storageBucket: "historia-acessivel-76f55.firebasestorage.app",
+  messagingSenderId: "456660035916",
+  appId: "1:456660035916:web:337c6ec488020d987d47e9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
